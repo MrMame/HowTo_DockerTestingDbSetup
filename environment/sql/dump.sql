1f39f3855a27930e2b9ddf6f8561dd0d6bc242c3
MrMame <mm@mail.de>
2023-05-30 22:03:59 +0200

    Add SQL File with Test commands and shell to run the sql file inside of the running postgres docker container

---
+++ environment/sql/dump.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE sqlTestDB;
+USE testDB;
+
+
+
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
